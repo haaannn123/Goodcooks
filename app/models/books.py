b38eb120bf4b5fb426f203452a3648e51f1113cc
haaannn123 <haaannn97@gmail.com>
2023-05-17 18:03:46 -0400

    changed constraints

--- app/models/books.py
+++ app/models/books.py
@@ -9,8 +9,8 @@ class Book(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(1000), nullable=False)
+    author = db.Column(db.String(1000), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     published = db.Column(db.Date, nullable=False)
