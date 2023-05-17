2a6ef0843ccadde69ae7b04566454a638e7a20f2
haaannn123 <haaannn97@gmail.com>
2023-05-17 19:24:54 -0400

    env.py changes

--- migrations/versions/e1c699e72f36_.py
+++ migrations/versions/e1c699e72f36_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: e1c699e72f36
-Revises: 
+Revises:
 Create Date: 2023-05-17 18:34:03.512299
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -68,6 +72,14 @@ def upgrade():
     sa.ForeignKeyConstraint(['bookshelf_id'], ['bookshelves.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE books SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE bookshelves SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE bookshelf_items SET SCHEMA {SCHEMA};")
+
     # ### end Alembic commands ###
 
 
