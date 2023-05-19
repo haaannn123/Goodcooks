e4e19523de893cb667ce1466dcd5d60f9a81c219
haaannn123 <haaannn97@gmail.com>
2023-05-19 13:55:57 -0400

    made sure migrations are up to date

--- app/api/book_routes.py
+++ app/api/book_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Book, db
 from app.forms import BookForm
-from datetime import date
+from datetime import date, datetime
 
 book_routes = Blueprint('books', __name__)
 
@@ -24,8 +24,10 @@ def book(id):
     return book.to_dict()
 
 @book_routes.route('/new', methods=['POST'])
-@login_required
 def create_book():
+    """
+    Create a new book
+    """
     form = BookForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
@@ -36,11 +38,44 @@ def create_book():
             price = form.data['price'],
             published = form.data['published'],
             preview_img = form.data['preview_img'],
-            created_at = date.today(),
-            updated_at = date.today()
         )
         print('BACKEnd', new_book)
         db.session.add(new_book)
         db.session.commit()
         return new_book.to_dict()
     return form.errors
+
+
+@book_routes.route('/<int:bookId>', methods=['PUT'])
+def update_book(bookId):
+    """
+    Update the book by it's id
+    """
+    book = Book.query.get(bookId)
+    data = request.get_json()
+
+    if book:
+        book.title = data['title']
+        book.author = data['author']
+        book.description = data['description']
+        book.price = data['price']
+
+        # Convert the date string to a Pythong date object
+        published_date = datetime.strptime(data['published'], '%Y-%m-%d').date()
+        book.published = published_date
+
+        book.preview_img = data['preview_img']
+        db.session.commit()
+        return book.to_dict()
+    return 'this didnt work!!!!'
+
+
+@book_routes.route('/<int:bookId>', methods=['DELETE'])
+def delete_book(bookId):
+    book = Book.query.get(bookId)
+    if (not book):
+        return ('No Book Found'), 404
+
+    db.session.delete(book)
+    db.session.commit()
+    return {'Book deleted!Yay!': bookId}
