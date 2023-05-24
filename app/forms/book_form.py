d0a090571b4bd33225ea4e0ae03765df99fa51ab
haaannn123 <haaannn97@gmail.com>
2023-05-23 22:10:14 -0400

    styling and fixed create book form

--- app/forms/book_form.py
+++ app/forms/book_form.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, NumberRange
 class BookForm(FlaskForm):
     title=StringField('Title', validators=[DataRequired(), Length(min=2)])
     author=StringField('Written By', validators=[DataRequired(), Length(min=2)])
-    description = StringField('Description', validators=[DataRequired(), Length(min=3)])
+    description = StringField('Description', validators=[DataRequired(), Length(min=30)])
     price = DecimalField('Price', places=2, rounding=None, number_format=3, validators=[DataRequired(), NumberRange(min=1)])
     published = DateField('Published',  format='%Y-%m-%d')
     preview_img= StringField('Image', validators=[DataRequired()])
