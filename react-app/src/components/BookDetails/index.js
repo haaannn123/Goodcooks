141613ae443cc57b9699dda039fd10b67643b9ad
haaannn123 <haaannn97@gmail.com>
2023-06-03 00:51:41 -0400

    reviews

--- react-app/src/components/BookDetails/index.js
+++ react-app/src/components/BookDetails/index.js
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import "./Book.css";
 import { useModal } from "../../context/Modal";
 import { thunkGetBookById } from "../../store/books";
 import { dateParser } from "../../helper_functions/dateParser";
@@ -9,12 +8,13 @@ import OpenModalButton from "../OpenModalButton";
 import AddToShelfModal from "../AddToShelfModal";
 import { thunkGetUserBookShelf } from "../../store/bookshelf";
 import Footer from "../Footer";
+import "./Book.css";
+import "./Reviews.css";
 
 const Book = () => {
-  const { bookId }  = useParams();
-  console.log('BOOK ID HERE', bookId)
+  const { bookId } = useParams();
   const dispatch = useDispatch();
-  const {closeModal } = useModal();
+  const { closeModal } = useModal();
 
   const book = useSelector((state) => state.booksReducer.book);
   dateParser(book.published);
@@ -26,34 +26,38 @@ const Book = () => {
 
   useEffect(() => {
     dispatch(thunkGetBookById(bookId));
-    dispatch(thunkGetUserBookShelf())
+    dispatch(thunkGetUserBookShelf());
   }, [dispatch, bookId]);
 
   return (
     <>
-    <div className="book-details-page">
-      <div className="book-img-add-to-shelf">
-      <img
-        className="cook-book-img"
-        src={book.preview_img}
-        alt="cookbook"
-      />
-      <OpenModalButton
-          buttonText="Add to shelf"
-          className="add-to-shelf-button"
-          onItemClick={closeModal}
-          modalComponent={<AddToShelfModal bookId={bookId}/>}
-      />
-      <button className="book-price-button">Kindle ${book.price}</button>
+      <div className="book-details-page">
+        <div className="book-img-add-to-shelf">
+          <img className="cook-book-img" src={book.preview_img} alt="cookbook" />
+          <OpenModalButton
+            buttonText="Add to shelf"
+            className="add-to-shelf-button"
+            onItemClick={closeModal}
+            modalComponent={<AddToShelfModal bookId={bookId} />}
+          />
+          <button className="book-price-button">Kindle ${book.price}</button>
+          <div className="stars-container">
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+          </div>
+          Rate this book
+        </div>
+        <div className="cook-book-info">
+          <h1>{book.title}</h1>
+          <h2>By {book.author}</h2>
+          <h4>First Published {formattedPublishedDate}</h4>
+          <p className="book-description">{book.description}</p>
+        </div>
       </div>
-      <div className="cook-book-info">
-        <h1>{book.title}</h1>
-        <h2>By {book.author}</h2>
-        <h4>{formattedPublishedDate}</h4>
-        <p className="book-description">{book.description}</p>
-      </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
