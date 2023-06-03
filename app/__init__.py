87cbaf3504be094e255c55e44ad2b87b63483246
haaannn123 <haaannn97@gmail.com>
2023-06-02 23:48:37 -0400

    reviews

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .api.book_routes import book_routes
 from .api.bookshelf_routes import bookshelf_routes
 from .api.bookshelf_item_routes import bookshelf_item_routes
+from .api.review_routes import review_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -34,6 +35,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(book_routes, url_prefix='/api/books')
 app.register_blueprint(bookshelf_routes, url_prefix='/api/bookshelves')
 app.register_blueprint(bookshelf_item_routes, url_prefix='/api/bookshelf_item')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 
 db.init_app(app)
 Migrate(app, db)
--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,14 +1,70 @@
-# from flask import Blueprint, jsonify, session, request
-# from flask_login import login_required
-# from app.models import Review, User, db
+from flask import Blueprint, jsonify, session, request
+from flask_login import login_required
+from app.models import Review, User, db
 
-# review_routes = Blueprint('reviews', __name__)
+review_routes = Blueprint('reviews', __name__)
 
-# @review_routes.route('/user/<int:user_id>')
+@review_routes.route('/product/<int:product_id>')
+def get_reviews_by_product_id(product_id):
+    product_reviews = Review.query.filter_by(product_id=product_id).all()
+    reviews = [review.to_dict() for review in product_reviews]
+
+    for review in reviews:
+        userId = review['user_id']
+        user = User.query.get(userId)
+        review_user = user.to_dict()
+        review['User_info'] = review_user
+
+    return reviews
+
+# @review_routes.route('/new/product/<int:product_id>', methods=['POST'])
 # @login_required
-# def reviews_of_user(user_id):
-#     """
-#     Query for all of the reviews of the current user
-#     """
-#     user_reviews = Review.query.filter_by(user_id=user_id).all()
-#     return [review.to_dict() for review in user_reviews]
+# def create_review_for_product_by_id(product_id):
+#     form = ReviewForm()
+#     owner_id = session.get('_user_id')
+#     print('FORM DATA:',form.data, product_id)
+#     form['csrf_token'].data = request.cookies["csrf_token"]
+
+#     if form.validate_on_submit():
+#         new_review = Review(
+#             user_id = owner_id,
+#             product_id = product_id,
+#             review = form.data['review'],
+#             rating = form.data['rating']
+#         )
+
+#         db.session.add(new_review)
+#         db.session.commit()
+#         return new_review.to_dict()
+#     return form.errors
+
+
+
+# @review_routes.route('/<int:id>', methods=['DELETE'])
+# @login_required
+# def delete_review(id):
+#     review = Review.query.get(id)
+#     if (not review):
+#         return ('No Review Found'), 404
+
+#     db.session.delete(review)
+#     db.session.commit()
+
+#     return {'Review Successfully Deleted': id}
+
+
+
+# @review_routes.route('/<int:review_id>/update', methods=['PUT'])
+# @login_required
+# def update_user_review(review_id):
+#     review = Review.query.get(review_id)
+#     data = request.get_json()
+#     # print('USER REVIEW--------------', review, data)
+
+#     if (review):
+#         review.rating = data["rating"]
+#         review.review = data["review"]
+
+#         db.session.commit()
+#         return review.to_dict()
+#     return {'MESSAGE': "Update review didn't work"}
