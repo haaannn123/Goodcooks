9493b79e3caea20638e8e70fb4eedad3a6a95e56
haaannn123 <haaannn97@gmail.com>
2023-06-15 14:13:31 -0400

    debug

--- app/models/follows.py
+++ app/models/follows.py
@@ -1,17 +1,20 @@
-from .db import db
+from .db import db, environment, add_prefix_for_prod, SCHEMA
 
 follows = db.Table(
     'follows',
     db.Column(
         "follower",
         db.Integer,
-        db.ForeignKey("users.id"),
+        db.ForeignKey(add_prefix_for_prod("users.id")),
         primary_key=True
     ),
     db.Column(
         'followed',
         db.Integer,
-        db.ForeignKey("users.id"),
+        db.ForeignKey(add_prefix_for_prod("users.id")),
         primary_key=True
     )
 )
+
+if environment == "production":
+    follows.schema = SCHEMA
