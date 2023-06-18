244e68bc1089397fd875e9d0c5ce756000f35027
haaannn123 <haaannn97@gmail.com>
2023-06-18 10:48:23 -0400

    profile-page;

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -28,33 +28,30 @@ const ProfilePage = () => {
   if (!user) return null;
 
   return (
-    <>
     <div className="profile-page-container page">
-      <div className="profile-info">
-        {user.profile_img ? (
-          <img src={user.profile_img} alt="user-profile" className="profile-img" />
-        ) : (
-          <img src="https://i.imgur.com/nqak9tT.png" alt="user-profile" className="profile-img" />
-        )}
-        <div className="names-names">
-            <h1 className="profile-page-user">{user.first_name} {user.last_name}</h1>
-            <span className="username">@ {user.username}</span>
-        </div>
-        {/* {renderFollowButton()} */}
-        <button className="follow-button" onClick={() => handleClick()}>{isFollowing ? "Unfollow": "Follow"}</button>
-      </div>
-      <div className="divider">
-            <hr className="silver-line"/>
+        <div className="profile-info">
+          {user.profile_img ? (
+            <img src={user.profile_img} alt="user-profile" className="profile-img" />
+          ) : (
+            <img src="https://i.imgur.com/nqak9tT.png" alt="user-profile" className="profile-img" />
+          )}
+          <div className="names-names">
+              <h1 className="profile-page-user">{user.first_name} {user.last_name}</h1>
+              <span className="username">@ {user.username}</span>
           </div>
-      <div className="user-bookshelf-info">
-        <h2 className="username-library">{user.first_name}'s Library</h2>
-        <NavLink to="/bookshelves/edit" className="see-all-button">
-          See All <span className="arrow"><i className="fa-solid fa-chevron-right"></i></span>
-        </NavLink>
-      </div>
+          <button className="follow-button" onClick={() => handleClick()}>{isFollowing ? "Unfollow": "Follow"}</button>
+        </div>
+        <div className="divider">
+              <hr className="silver-line"/>
+            </div>
+        <div className="user-bookshelf-info">
+          <h2 className="username-library">{user.first_name}'s Library</h2>
+          <NavLink to="/bookshelves/edit" className="see-all-button">
+            See All <span className="arrow"><span class="material-symbols-outlined">chevron_right</span></span>
+          </NavLink>
+        </div>
       <BookShelf firstName={user.first_name} lastName={user.last_name} />
     </div>
-    </>
   );
 };
 export default ProfilePage;
